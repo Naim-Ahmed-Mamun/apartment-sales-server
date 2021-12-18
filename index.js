ad3aa5e699ad6ca77e6525bbc3ff6928a203b3e4
Naim-Ahmed-Mamun <naimahmod852@gmail.com>
2021-12-18 18:37:55 +0600

    some change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const fileUpload = require('express-fileupload');
 const ObjectId = require('mongodb').ObjectId;
 const PORT = process.env.PORT || 5000;
 
-// middleware
+// add middleware
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 app.use(fileUpload());
